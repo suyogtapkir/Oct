cc765d11d93c10695a257b45a52953dcef2cb66a
suyog <suyogtapkir1152@gmail.com>
2024-10-07 18:44:30 +0530

    commit from feature branch for jenkins trigger

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,5 +6,6 @@ package com.mycompany.app;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World Suyog! From feature branch");
+        System.out.println("jenkin trigger");
     }
 }

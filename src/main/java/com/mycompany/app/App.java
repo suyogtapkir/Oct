d6e5779e2e717d06b082a1c73012ea1911c4b424
suyogtapkir <suyogtapkir1152@gmail.com>
2024-10-20 08:02:18 +0530

    test webhook changes

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -5,6 +5,6 @@ package com.mycompany.app;
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World Suyog!");
+        System.out.println("Hello World Suyog! Test web hook");
     }
 }
